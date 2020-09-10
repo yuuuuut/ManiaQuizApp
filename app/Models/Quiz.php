5bbf5dc03f9d11ccb635aeb14090a512173f9508
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-09-11 00:49:13 +0900

    [update] quiz.index Userで表示分け

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Auth;
+
 class Quiz extends Model
 {
     protected $fillable = [
--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div>
     @foreach($quizzes as $quiz)
         {{ $quiz->content }}
         <a href="{{ route('quiz.index', ['category_id' => $quiz->category->id]) }}">{{ $quiz->category->name }}</a>
-        <a href="/quiz/{{ $quiz->id }}">回答する</a>
+        @if($quiz->user_id === Auth::id())
+            <a href="/quiz/{{ $quiz->id }}">回答一覧を見る</a>
+        @else
+            <a href="/quiz/{{ $quiz->id }}">回答する</a>
+        @endif
         <br>
     @endforeach
 
