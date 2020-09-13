9b8f1c6fffdd229337d659c709ff50fdbe5663c1
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-09-13 15:57:14 +0900

    [update] Notification iindex.blade

--- resources/views/notification/index.blade.php
+++ resources/views/notification/index.blade.php
@@ -15,6 +15,10 @@
         <a href="{{ route('quiz.show', ['quiz' => $notifi->quiz_id]) }}">
             あなたの回答が正解に選ばれました。
         </a>
+    @elseif($notifi->action === 'NoneBestAnswer')
+        <a href="{{ route('quiz.show', ['quiz' => $notifi->quiz_id]) }}">
+            あなたが回答した問題の正解が決定しました.
+        </a>
     @endif
 @endforeach
 
