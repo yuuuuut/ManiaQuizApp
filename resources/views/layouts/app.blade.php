d0974a3b4d109d9c0a287ed1591d21352eadff8a
Sekiguchi Yuto <yutodazepa@icloud.com>
2020-09-21 13:46:34 +0900

    [style] app.blade.php change style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,32 +3,30 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="
+            fixed-top
+            footer-color
+            navbar
+            navbar-expand-md
+            shadow-sm
+        ">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -69,6 +67,30 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer class="
+            fixed-bottom
+            bd-highlight
+        ">
+            <nav class="
+                footer-color
+                d-flex
+                justify-content-around
+                navbar
+                navbar-expand-md
+            ">
+                <div class="d-flex flex-column">
+                    <a href="/">
+                        <i class="fas fa-home fa-2x footer-icon--color"></i>
+                    </a>
+                    <div class="footer-icon--text">ホーム</div>
+                </div>
+                <a href="/"><i class="fas fa-user fa-2x"></i></a>
+                <a href="/"><i class="fas fa-trophy fa-2x"></i></a>
+                <a href="/"><i class="fas fa-eye fa-2x"></i></a>
+                <a href="/" ><i class="far fa-grin-stars fa-2x"></i></a>
+            </nav>
+        </footer>
     </div>
 </body>
 </html>
--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -4,6 +4,7 @@
 
 <div>
     @foreach($quizzes as $quiz)
+    <div style="margin-bottom: 1000px;">
         {{ $quiz->content }}
         <a href="{{ route('quiz.index', ['category_id' => $quiz->category->id]) }}">{{ $quiz->category->name }}</a>
         @if($quiz->user_id === Auth::id())
@@ -12,6 +13,7 @@
             <a href="/quiz/{{ $quiz->id }}">回答する</a>
         @endif
         <br>
+    </div>
     @endforeach
 
     {{ $quizzes->appends(['category_id' => $category_id])->links() }}
